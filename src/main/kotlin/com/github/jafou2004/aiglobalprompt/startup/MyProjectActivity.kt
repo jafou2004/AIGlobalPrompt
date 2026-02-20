cb4a34bc602ef6239cac1068a46228ea1db9f90c
GitHub Action <action@github.com>
2026-02-20 12:10:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/jafou2004/aiglobalprompt/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/jafou2004/aiglobalprompt/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.jafou2004.aiglobalprompt.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
